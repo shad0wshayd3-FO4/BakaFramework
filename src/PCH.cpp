a2f5e505f3d194cf320e5e4799e1325002010553
shad0wshayd3 <shad0wshayd3@users.noreply.github.com>
2021-05-10 04:35:46 -0600

    update precompiled header

--- src/PCH.cpp
+++ src/PCH.cpp
@@ -1,21 +1,41 @@
 #include "PCH.h"
 
-#include <Windows.h>
+#ifdef F4SE_SUPPORT_XBYAK
+#include <xbyak/xbyak.h>
 
-#undef GetComputerName
-
-namespace F4SE
+namespace stl
 {
-	namespace WinAPI
+	namespace detail
 	{
-		bool(GetComputerName)(
-			char* a_computerName,
-			std::uint32_t* a_size) noexcept
+		struct asm_patch :
+			Xbyak::CodeGenerator
 		{
-			return static_cast<bool>(
-				::GetComputerNameA(
-					static_cast<::LPSTR>(a_computerName),
-					reinterpret_cast<::LPDWORD>(a_size)));
-		}
+			asm_patch(std::uintptr_t a_dst)
+			{
+				Xbyak::Label dst;
+
+				jmp(ptr[rip + dst]);
+
+				L(dst);
+				dq(a_dst);
+			}
+		};
+	}
+
+	void asm_jump(std::uintptr_t a_from, std::size_t a_size, std::uintptr_t a_to)
+	{
+		detail::asm_patch p{ a_to };
+		p.ready();
+		assert(p.getSize() <= a_size);
+		REL::safe_write(
+			a_from,
+			std::span{ p.getCode<const std::byte*>(), p.getSize() });
+	}
+
+	void asm_replace(std::uintptr_t a_from, std::size_t a_size, std::uintptr_t a_to)
+	{
+		REL::safe_fill(a_from, REL::INT3, a_size);
+		asm_jump(a_from, a_size, a_to);
 	}
 }
+#endif
--- src/PCH.h
+++ src/PCH.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#pragma warning(push)
 #include "F4SE/F4SE.h"
 #include "RE/Fallout.h"
 
@@ -9,33 +10,24 @@
 #include <string_view>
 #include <vector>
 
-#include <fmt/chrono.h>
 #include <spdlog/sinks/basic_file_sink.h>
-
-#include "AutoTOML.hpp"
+#include <fmt/format.h>
+#include <AutoTOML.hpp>
+#pragma warning(pop)
 
 #define DLLEXPORT __declspec(dllexport)
 
-namespace F4SE
-{
-	namespace WinAPI
-	{
-		inline constexpr auto(MAX_COMPUTERNAME_LENGTH){ static_cast<std::uint32_t>(0x0F) };
-
-		[[nodiscard]] bool(GetComputerName)(
-			char* a_computerName,
-			std::uint32_t* a_size) noexcept;
-	}
-}
-
-namespace logger
-{
-	using namespace F4SE::log;
-}
+namespace logger = F4SE::log;
 
 namespace stl
 {
+	using F4SE::stl::emplace_vtable;
 	using F4SE::stl::report_and_fail;
+	using F4SE::stl::to_underlying;
+
+#ifdef F4SE_SUPPORT_XBYAK
+	void asm_replace(std::uintptr_t a_from, std::size_t a_size, std::uintptr_t a_to);
+#endif
 }
 
 using namespace std::literals;
