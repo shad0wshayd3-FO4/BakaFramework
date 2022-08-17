b2918c2439af43d342e92d9e4d72283676d1a990
shad0wshayd3 <shad0wshayd3@users.noreply.github.com>
2022-08-17 14:01:16 -0600

    remove inventoryweight changes for non-player refs

--- src/c++/Events/Handlers/TESContainerChanged.h
+++ src/c++/Events/Handlers/TESContainerChanged.h
@@ -4,37 +4,6 @@
 
 namespace Events::Handlers
 {
-	namespace
-	{
-		class WeightUpdate :
-			public F4SE::ITaskDelegate
-		{
-		public:
-			WeightUpdate(RE::TESObjectREFR* a_refr) :
-				m_refr(a_refr)
-			{}
-
-			virtual ~WeightUpdate()
-			{
-				m_refr.reset();
-			}
-
-			virtual void Run() override
-			{
-				if (auto refr = m_refr.get())
-				{
-					if (auto inventoryWeight = Forms::InventoryWeight_DO->GetForm<RE::ActorValueInfo>())
-					{
-						refr->SetBaseActorValue(*inventoryWeight, refr->GetWeightInContainer());
-					}
-				}
-			}
-
-		protected:
-			RE::BSTSmartPointer<RE::TESObjectREFR, RE::BSTSmartPointerGamebryoRefCount> m_refr;
-		};
-	}
-
 	class TESContainerChangedEventHandler :
 		public RE::BSTEventSink<RE::TESContainerChangedEvent>
 	{
@@ -47,16 +16,15 @@ namespace Events::Handlers
 
 		virtual RE::BSEventNotifyControl ProcessEvent(const RE::TESContainerChangedEvent& a_event, RE::BSTEventSource<RE::TESContainerChangedEvent>*) override
 		{
-			auto source = RE::TESForm::GetFormByID(a_event.oldContainerFormID);
-			if (auto refr = source ? source->As<RE::TESObjectREFR>() : nullptr)
+			if (a_event.newContainerFormID == 0x14 || a_event.oldContainerFormID == 0x14)
 			{
-				F4SE::GetTaskInterface()->AddTask(new WeightUpdate(refr));
-			}
-
-			auto target = RE::TESForm::GetFormByID(a_event.newContainerFormID);
-			if (auto refr = target ? target->As<RE::TESObjectREFR>() : nullptr)
-			{
-				F4SE::GetTaskInterface()->AddTask(new WeightUpdate(refr));
+				if (auto InventoryWeight = Forms::InventoryWeight_DO->GetForm<RE::ActorValueInfo>())
+				{
+					auto PlayerCharacter = RE::PlayerCharacter::GetSingleton();
+					PlayerCharacter->SetBaseActorValue(
+						*InventoryWeight,
+						PlayerCharacter->GetWeightInContainer());
+				}
 			}
 
 			return RE::BSEventNotifyControl::kContinue;
