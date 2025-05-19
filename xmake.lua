8eb75f1b62baa6aa87bdfda7555e1db0ae9ebd84
shad0wshayd3 <2724172+shad0wshayd3@users.noreply.github.com>
2025-05-19 02:02:56 -0600

    refactor: project update

--- xmake.lua
+++ xmake.lua
@@ -1,7 +1,7 @@
--- set xmake version
+-- set minimum xmake version
 set_xmakever("2.9.4")
 
--- include local folders
+-- includes
 includes("lib/commonlibf4")
 
 -- set project
@@ -10,23 +10,21 @@ set_version("3.0.0")
 set_license("GPL-3.0")
 
 -- set defaults
-set_arch("x64")
 set_languages("c++23")
-set_optimize("faster")
-set_warnings("allextra", "error")
-set_defaultmode("releasedbg")
+set_warnings("allextra")
 
--- enable lto
+-- set policies
 set_policy("build.optimization.lto", true)
+set_policy("package.requires_lock", true)
 
 -- add rules
 add_rules("mode.debug", "mode.releasedbg")
 add_rules("plugin.vsxmake.autoupdate")
 
--- add config
+-- set config
 set_config("rex_ini", true)
 
--- setup targets
+-- targets
 target("BakaFramework")
     -- bind local dependencies
     add_deps("commonlibf4")
@@ -37,11 +35,11 @@ target("BakaFramework")
         author = "shad0wshayd3"
     })
 
-    -- add source files
+    -- add src files
     add_files("src/**.cpp")
     add_headerfiles("src/**.h")
     add_includedirs("src")
-    set_pcxxheader("src/PCH.h")
+    set_pcxxheader("src/pch.h")
 
     -- add extra files
     add_extrafiles(".clang-format")
